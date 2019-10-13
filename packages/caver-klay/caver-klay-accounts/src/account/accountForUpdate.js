dda07a75ca45cca5b8c6305588ec22604b15e6c3
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2019-10-14 08:28:32 +0900

    Implement structures for Account/AccountForUpdate

---
+++ packages/caver-klay/caver-klay-accounts/src/account/accountForUpdate.js
@@ -0,0 +1,69 @@
+const isValidRole = require('../../../../caver-utils').isValidRole
+
+class AccountForUpdate {
+	constructor(address, keyForUpdate, options) {
+		this.address = address
+		this.keyForUpdate = keyFormatter(keyForUpdate, options)
+	}
+
+	fillUpdateObject(updateObject) {
+		delete updateObject.key
+		Object.assign(updateObject, this.keyForUpdate)
+	}
+}
+
+function keyFormatter(keyForUpdate, options) {
+	const keyObject = {}
+
+	if (typeof keyForUpdate === 'string') {
+		if (options) throw new Error(`Failed to keyFormatter for AccountForUpdate: AccountKeyPublic/legacyKey/failKey cannot have options`)
+		switch(keyForUpdate) {
+			case 'legacyKey':
+				keyObject.legacyKey = true
+				break
+			case 'failKey':
+				keyObject.failKey = true
+				break
+			default:
+				keyObject.publicKey = keyForUpdate
+				break
+		}
+	} else if (Array.isArray(keyForUpdate)) {
+		if (!options || !options.threshold || !options.weight) throw new Error('For AccountKeyMultiSig, threshold and weight should be defined in options object.')
+		if (!Array.isArray(options.weight)) throw new Error('The weight should be defined as a array.')
+		if (options.weight.length !== keyForUpdate.length) throw new Error('The length of keys in AccountKeyMultiSig and the length of weight array do not match.')
+		
+		keyObject.multisig = {
+			threshold: options.threshold,
+			keys: []
+		}
+
+		let weightSum = 0
+
+		for (let i = 0; i < keyForUpdate.length; i ++) {
+			const key = keyForUpdate[i]
+			keyObject.multisig.keys.push({weight: options.weight[i], publicKey: key})
+			weightSum += options.weight[i]
+		}
+
+		if (weightSum < options.threshold) throw new Error(`Invalid options for AccountKeyMultiSig: The sum of weights is less than the threshold.`)
+	} else {
+		for (let key in keyForUpdate) {
+			if (!isValidRole(key)) throw new Error(`Invalid role is defined: ${key}`)
+			options = options || {}
+			if (key === 'transactionKey') {
+				keyObject.roleTransactionKey = keyFormatter(keyForUpdate[key], options.transactionKey)
+			}
+			if (key === 'updateKey') {
+				keyObject.roleAccountUpdateKey = keyFormatter(keyForUpdate[key], options.updateKey)
+			}
+			if (key === 'feePayerKey') {
+				keyObject.roleFeePayerKey = keyFormatter(keyForUpdate[key], options.feePayerKey)
+			}
+		}
+	}
+	
+	return keyObject
+}
+
+module.exports = AccountForUpdate
