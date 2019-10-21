9bdf374875f5f3b832ac18a2c44ddf3ff2ce2b25
Junghyun Colin Kim <colin.kim@groundx.xyz>
2019-10-22 08:19:58 +0900

    Fix
    1. Fetch tags from upstream first
    2. treat the case of the first rc version

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Fetch tags from upstream first
+git fetch -t git@github.com:klaytn/caver-js.git
+
 VERSION=$(cat package.json | jq -r '.["version"]')
 echo "Trying to tag v$VERSION"
 git tag | grep "$VERSION\$"
@@ -9,7 +13,10 @@ if [ $? -eq 0 ]; then
 fi
 
 PREV_RCVERSION_FULL=`git tag | grep "$VERSION" | sort | tail -1`
-RCSUFFIX=$(echo "${PREV_RCVERSION_FULL##*.}"+1 | bc)
+RCSUFFIX="1"
+if [ ! -z "$PREV_RCVERSION_FULL" ]; then
+  RCSUFFIX=$(echo "${PREV_RCVERSION_FULL##*.}"+1 | bc)
+fi
 
 RCVERSION="v$VERSION-rc.$RCSUFFIX"
 echo "tagging $RCVERSION"
