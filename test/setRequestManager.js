0607a402154e3381497e68e27dd6024d1d47844d
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2020-03-04 09:59:14 +0900

    Implemented test code for using setRequestManger

---
+++ test/setRequestManager.js
@@ -0,0 +1,55 @@
+/*
+    Copyright 2020 The caver-js Authors
+    This file is part of the caver-js library.
+
+    The caver-js library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The caver-js library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the caver-js. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+chai.use(sinonChai)
+
+const expect = chai.expect
+
+const Caver = require('../index.js')
+const testRPCURL = require('./testrpc')
+const { Manager } = require('../packages/caver-core-requestmanager/src/index')
+
+describe('setRequestManager', () => {
+    it('CAVERJS-UNIT-ETC-203: should call setRequestManager with each pacakge instead of setProvider', () => {
+        const setProviderSpy = sinon.spy(Manager.prototype, 'setProvider')
+
+        const caver = new Caver(testRPCURL)
+
+        expect(caver).not.to.be.undefined
+        expect(setProviderSpy).to.have.been.calledOnce
+
+        const setKlayRequestManager = sinon.spy(caver.klay, 'setRequestManager')
+        const setNetRequestManager = sinon.spy(caver.klay.net, 'setRequestManager')
+        const setPersonalRequestManager = sinon.spy(caver.klay.personal, 'setRequestManager')
+        const setAccountsRequestManager = sinon.spy(caver.klay.accounts, 'setRequestManager')
+        const setKlayProvider = sinon.spy(caver.klay, 'setProvider')
+
+        const newProvider = new Caver.providers.HttpProvider('https://api.baobab.klaytn.net:8651/')
+        caver.setProvider(newProvider)
+
+        expect(setKlayRequestManager).to.have.been.calledOnce
+        expect(setNetRequestManager).to.have.been.calledOnce
+        expect(setPersonalRequestManager).to.have.been.calledOnce
+        expect(setAccountsRequestManager).to.have.been.calledOnce
+        expect(setKlayProvider).not.to.have.been.calledOnce
+    })
+})
