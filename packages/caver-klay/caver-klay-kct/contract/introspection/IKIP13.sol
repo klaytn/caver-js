6719be7ab736df234044a1b24776842dd9fa09c1
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2020-03-06 16:51:01 +0900

    Edited description

--- packages/caver-klay/caver-klay-kct/contract/introspection/IKIP13.sol
+++ packages/caver-klay/caver-klay-kct/contract/introspection/IKIP13.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
 
 /**
- * @dev Interface of the KIP13 standard, as defined in the
- * [KIP](https://klaytn.github.io/kips/KIPs/kip-13-interface_query_standard).
+ * @dev Interface of the KIP-13 standard, as defined in the
+ * [KIP-13](https://klaytn.github.io/kips/KIPs/kip-13-interface_query_standard).
  *
  * Implementers can declare support of contract interfaces, which can then be
  * queried by others.
@@ -13,7 +13,7 @@ interface IKIP13 {
     /**
      * @dev Returns true if this contract implements the interface defined by
      * `interfaceId`. See the corresponding
-     * [KIP section](https://klaytn.github.io/kips/KIPs/kip-13-interface_query_standard#how-interface-identifiers-are-defined)
+     * [KIP-13 section](https://klaytn.github.io/kips/KIPs/kip-13-interface_query_standard#how-interface-identifiers-are-defined)
      * to learn more about how these ids are created.
      *
      * This function call must use less than 30 000 gas.
