a75e10b866e7b6424a250de251a2b6d061b61cc8
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2020-03-06 13:08:40 +0900

    Edited pausable interface id

--- packages/caver-klay/caver-klay-kct/contract/token/KIP7/KIP7Pausable.sol
+++ packages/caver-klay/caver-klay-kct/contract/token/KIP7/KIP7Pausable.sol
@@ -13,12 +13,13 @@ contract KIP7Pausable is KIP13, KIP7, Pausable {
      *     bytes4(keccak256('paused()')) == 0x5c975abb
      *     bytes4(keccak256('pause()')) == 0x8456cb59
      *     bytes4(keccak256('unpause()')) == 0x3f4ba83a
+     *     bytes4(keccak256('isPauser(address)')) == 0x46fbf68e
      *     bytes4(keccak256('addPauser(address)')) == 0x82dc1ec4
      *     bytes4(keccak256('renouncePauser()')) == 0x6ef8d66d
      *
-     *     => 0x5c975abb ^ 0x8456cb59 ^ 0x3f4ba83a ^ 0x82dc1ec4 ^ 0x6ef8d66d == 0x0baef171
+     *     => 0x5c975abb ^ 0x8456cb59 ^ 0x3f4ba83a ^ 0x46fbf68e ^ 0x82dc1ec4 ^ 0x6ef8d66d == 0x4d5507ff
      */
-    bytes4 private constant _INTERFACE_ID_KIP7PAUSABLE = 0x0baef171;
+    bytes4 private constant _INTERFACE_ID_KIP7PAUSABLE = 0x4d5507ff;
 
     constructor () public {
         // register the supported interfaces to conform to KIP17 via KIP13
