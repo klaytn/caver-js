c492b6e93f3cd315e6f3de2b5efe9b7722e37164
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2020-04-17 13:39:09 +0900

    Implemented various AccountKey types for Account

---
+++ packages/caver-account/src/accountKey/accountKeyPublic.js
@@ -0,0 +1,91 @@
+/*
+    Copyright 2020 The caver-js Authors
+    This file is part of the caver-js library.
+
+    The caver-js library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The caver-js library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the caver-js. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+const RLP = require('eth-lib/lib/rlp')
+const utils = require('../../../caver-utils')
+const { ACCOUNT_KEY_TAG } = require('./accountKeyHelper')
+
+/**
+ * Representing an AccountKeyPublic.
+ * @class
+ */
+class AccountKeyPublic {
+    /**
+     * Decodes an RLP-encoded AccountKeyPublic string.
+     * @param {string} rlpEncodedKey - An RLP-encoded AccountKeyPublic string.
+     * @return {AccountKeyPublic}
+     */
+    static decode(rlpEncodedKey) {
+        rlpEncodedKey = utils.addHexPrefix(rlpEncodedKey)
+        if (!rlpEncodedKey.startsWith(ACCOUNT_KEY_TAG.ACCOUNT_KEY_PUBLIC_TAG))
+            throw new Error(`Cannot decode to AccountKeyPublic: ${rlpEncodedKey}`)
+
+        const publicKey = RLP.decode(`0x${rlpEncodedKey.slice(ACCOUNT_KEY_TAG.ACCOUNT_KEY_PUBLIC_TAG.length)}`)
+        return new AccountKeyPublic(publicKey)
+    }
+
+    static fromXYPoint(x, y) {
+        const pubKey = utils.addHexPrefix(x) + utils.stripHexPrefix(y)
+        return new AccountKeyPublic(pubKey)
+    }
+
+    /**
+     * Create an instance of AccountKeyPublic.
+     * @param {string} publicKey - The address of account.
+     */
+    constructor(publicKey) {
+        this._publicKey = publicKey
+    }
+
+    /**
+     * @type {string}
+     */
+    get publicKey() {
+        return this._publicKey
+    }
+
+    set publicKey(p) {
+        if (!utils.isValidPublicKey(p)) throw new Error(`Invalid public key: ${p}`)
+        this._publicKey = p
+    }
+
+    /**
+     * Returns an RLP-encoded AccountKeyPublic string.
+     * @return {string}
+     */
+    getRLPEncoding() {
+        const compressedPublicKey = utils.compressPublicKey(this.publicKey)
+        return ACCOUNT_KEY_TAG.ACCOUNT_KEY_PUBLIC_TAG + RLP.encode(compressedPublicKey).slice(2)
+    }
+
+    /**
+     * Returns the x and y coordinates of publicKey.
+     * @return {object}
+     */
+    getXYPoint() {
+        const publicKeyString = this.publicKey.replace('0x', '')
+        if (publicKeyString.length !== 128) throw Error('Invalid public key')
+
+        const x = `0x${publicKeyString.slice(0, 64).replace(/^0+/, '')}`
+        const y = `0x${publicKeyString.slice(64).replace(/^0+/, '')}`
+
+        return { x, y }
+    }
+}
+
+module.exports = AccountKeyPublic
---
+++ packages/caver-account/src/accountKey/accountKeyWeightedMultiSig.js
@@ -0,0 +1,117 @@
+/*
+    Copyright 2020 The caver-js Authors
+    This file is part of the caver-js library.
+
+    The caver-js library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The caver-js library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the caver-js. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+const RLP = require('eth-lib/lib/rlp')
+const Bytes = require('eth-lib/lib/bytes')
+const WeightedPublicKey = require('./weightedPublicKey')
+const utils = require('../../../caver-utils')
+const { ACCOUNT_KEY_TAG } = require('./accountKeyHelper')
+
+/**
+ * Representing an AccountKeyWeightedMultiSig.
+ * @class
+ */
+class AccountKeyWeightedMultiSig {
+    /**
+     * Decodes an RLP-encoded AccountKeyWeightedMultiSig string.
+     * @param {string} rlpEncodedKey - An RLP-encoded AccountKeyWeightedMultiSig string.
+     * @return {AccountKeyWeightedMultiSig}
+     */
+    static decode(rlpEncodedKey) {
+        rlpEncodedKey = utils.addHexPrefix(rlpEncodedKey)
+        if (!rlpEncodedKey.startsWith(ACCOUNT_KEY_TAG.ACCOUNT_KEY_WEIGHTED_MULTISIG_TAG))
+            throw new Error(`Cannot decode to AccountKeyWeightedMultiSig: ${rlpEncodedKey}`)
+
+        const [threshold, multiSigkeys] = RLP.decode(`0x${rlpEncodedKey.slice(ACCOUNT_KEY_TAG.ACCOUNT_KEY_WEIGHTED_MULTISIG_TAG.length)}`)
+        const weightedPublicKeys = multiSigkeys.map(wieghtedPublicKey => {
+            return new WeightedPublicKey(wieghtedPublicKey[0], wieghtedPublicKey[1])
+        })
+        return new AccountKeyWeightedMultiSig(threshold, weightedPublicKeys)
+    }
+
+    /**
+     * Creates an instance of AccountKeyWeighedMultiSig.
+     * @param {Array.<string>} publicKeyArray - An array of public key strings.
+     * @param {object} options - An options which defines threshold and weight.
+     * @return {AccountKeyWeightedMultiSig}
+     */
+    static fromPublicKeysAndOptions(publicKeyArray, options) {
+        const weightedPublicKeys = []
+        for (let i = 0; i < publicKeyArray.length; i++) {
+            const weightedPublicKey = new WeightedPublicKey(options.weight[i], publicKeyArray[i])
+            weightedPublicKeys.push(weightedPublicKey)
+        }
+        return new AccountKeyWeightedMultiSig(options.threshold, weightedPublicKeys)
+    }
+
+    /**
+     * Create an instance of AccountKeyWeightedMultiSig.
+     * @param {number} threshold - The threshold of accountKey.
+     * @param {Array.<WeightedPublicKey>} weightedPublicKeys - An array of instances of WeightedPublicKeys
+     */
+    constructor(threshold, weightedPublicKeys) {
+        this._threshold = utils.hexToNumber(threshold)
+        this._weightedPublicKeys = weightedPublicKeys || []
+    }
+
+    /**
+     * @type {Number}
+     */
+    get threshold() {
+        return this._threshold
+    }
+
+    set threshold(t) {
+        this._threshold = utils.hexToNumber(t)
+    }
+
+    /**
+     * @type {Array.<WeightedPublicKey>}
+     */
+    get weightedPublicKeys() {
+        return this._weightedPublicKeys
+    }
+
+    set weightedPublicKeys(wps) {
+        for (const wp of wps) {
+            if (!(wp instanceof WeightedPublicKey)) throw new Error(`Invalid type of weighted public key.`)
+        }
+        this._weightedPublicKeys = wps
+    }
+
+    /**
+     * Returns an RLP-encoded AccountKeyWeightedMultiSig string.
+     * @return {string}
+     */
+    getRLPEncoding() {
+        if (this.threshold === undefined) throw new Error('threshold should be specified for multisig account')
+        if (this.weightedPublicKeys.length === 0) throw new Error('weightedPublicKeys should be specified for multisig account')
+
+        const encodedMultisigPublicKeys = []
+        for (const weightedPublicKey of this.weightedPublicKeys) {
+            encodedMultisigPublicKeys.push(weightedPublicKey.encodeToBytes())
+        }
+
+        return (
+            ACCOUNT_KEY_TAG.ACCOUNT_KEY_WEIGHTED_MULTISIG_TAG +
+            RLP.encode([Bytes.fromNat(utils.numberToHex(this.threshold)), encodedMultisigPublicKeys]).slice(2)
+        )
+    }
+}
+
+module.exports = AccountKeyWeightedMultiSig
