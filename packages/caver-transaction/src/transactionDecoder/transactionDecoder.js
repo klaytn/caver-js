0c50a4e48b95d09cc0fe1571b08a4021c7ebc456
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2020-05-20 12:20:37 +0900

    Added Transaction types to export

--- packages/caver-transaction/src/transactionDecoder/transactionDecoder.js
+++ packages/caver-transaction/src/transactionDecoder/transactionDecoder.js
@@ -20,13 +20,25 @@ const { TX_TYPE_STRING, typeDetectionFromRLPEncoding } = require('../transaction
 const LegacyTransaction = require('../transactionTypes/legacyTransaction/legacyTransaction')
 const ValueTransfer = require('../transactionTypes/valueTransfer/valueTransfer')
 const FeeDelegatedValueTransfer = require('../transactionTypes/valueTransfer/feeDelegatedValueTransfer')
+const FeeDelegatedValueTransferWithRatio = require('../transactionTypes/valueTransfer/feeDelegatedValueTransferWithRatio')
 const ValueTransferMemo = require('../transactionTypes/valueTransferMemo/valueTransferMemo')
 const FeeDelegatedValueTransferMemo = require('../transactionTypes/valueTransferMemo/feeDelegatedValueTransferMemo')
+const FeeDelegatedValueTransferMemoWithRatio = require('../transactionTypes/valueTransferMemo/feeDelegatedValueTransferMemoWithRatio')
 const AccountUpdate = require('../transactionTypes/accountUpdate/accountUpdate')
+const FeeDelegatedAccountUpdate = require('../transactionTypes/accountUpdate/feeDelegatedAccountUpdate')
+const FeeDelegatedAccountUpdateWithRatio = require('../transactionTypes/accountUpdate/feeDelegatedAccountUpdateWithRatio')
 const SmartContractDeploy = require('../transactionTypes/smartContractDeploy/smartContractDeploy')
+const FeeDelegatedSmartContractDeploy = require('../transactionTypes/smartContractDeploy/feeDelegatedSmartContractDeploy')
+const FeeDelegatedSmartContractDeployWithRatio = require('../transactionTypes/smartContractDeploy/feeDelegatedSmartContractDeployWithRatio')
 const SmartContractExecution = require('../transactionTypes/smartContractExecution/smartContractExecution')
+const FeeDelegatedSmartContractExecution = require('../transactionTypes/smartContractExecution/feeDelegatedSmartContractExecution')
+const FeeDelegatedSmartContractExecutionWithRatio = require('../transactionTypes/smartContractExecution/feeDelegatedSmartContractExecutionWithRatio')
 const Cancel = require('../transactionTypes/cancel/cancel')
+const FeeDelegatedCancel = require('../transactionTypes/cancel/feeDelegatedCancel')
+const FeeDelegatedCancelWithRatio = require('../transactionTypes/cancel/feeDelegatedCancelWithRatio')
 const ChainDataAnchoring = require('../transactionTypes/chainDataAnchoring/chainDataAnchoring')
+const FeeDelegatedChainDataAnchoring = require('../transactionTypes/chainDataAnchoring/feeDelegatedChainDataAnchoring')
+const FeeDelegatedChainDataAnchoringWithRatio = require('../transactionTypes/chainDataAnchoring/feeDelegatedChainDataAnchoringWithRatio')
 
 /**
  * Representing a transaction decoder.
@@ -48,20 +60,44 @@ class TransactionDecoder {
                 return ValueTransfer.decode(rlpEncoded)
             case TX_TYPE_STRING.TxTypeFeeDelegatedValueTransfer:
                 return FeeDelegatedValueTransfer.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedValueTransferWithRatio:
+                return FeeDelegatedValueTransferWithRatio.decode(rlpEncoded)
             case TX_TYPE_STRING.TxTypeValueTransferMemo:
                 return ValueTransferMemo.decode(rlpEncoded)
             case TX_TYPE_STRING.TxTypeFeeDelegatedValueTransferMemo:
                 return FeeDelegatedValueTransferMemo.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedValueTransferMemoWithRatio:
+                return FeeDelegatedValueTransferMemoWithRatio.decode(rlpEncoded)
             case TX_TYPE_STRING.TxTypeAccountUpdate:
                 return AccountUpdate.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedAccountUpdate:
+                return FeeDelegatedAccountUpdate.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedAccountUpdateWithRatio:
+                return FeeDelegatedAccountUpdateWithRatio.decode(rlpEncoded)
             case TX_TYPE_STRING.TxTypeSmartContractDeploy:
                 return SmartContractDeploy.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedSmartContractDeploy:
+                return FeeDelegatedSmartContractDeploy.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedSmartContractDeployWithRatio:
+                return FeeDelegatedSmartContractDeployWithRatio.decode(rlpEncoded)
             case TX_TYPE_STRING.TxTypeSmartContractExecution:
                 return SmartContractExecution.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedSmartContractExecution:
+                return FeeDelegatedSmartContractExecution.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedSmartContractExecutionWithRatio:
+                return FeeDelegatedSmartContractExecutionWithRatio.decode(rlpEncoded)
             case TX_TYPE_STRING.TxTypeCancel:
                 return Cancel.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedCancel:
+                return FeeDelegatedCancel.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedCancelWithRatio:
+                return FeeDelegatedCancelWithRatio.decode(rlpEncoded)
             case TX_TYPE_STRING.TxTypeChainDataAnchoring:
                 return ChainDataAnchoring.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedChainDataAnchoring:
+                return FeeDelegatedChainDataAnchoring.decode(rlpEncoded)
+            case TX_TYPE_STRING.TxTypeFeeDelegatedChainDataAnchoringWithRatio:
+                return FeeDelegatedChainDataAnchoringWithRatio.decode(rlpEncoded)
         }
     }
 }
