9f5fda1fefd10e6230dacc3e4bb6482b9cd29fe8
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2020-06-15 08:38:56 +0900

    Modified isEmptySig

--- packages/caver-utils/src/utils.js
+++ packages/caver-utils/src/utils.js
@@ -929,21 +929,27 @@ const isValidRole = role => {
     return false
 }
 
+// ['0x01', '0x', '0x]
+// [['0x01', '0x', '0x]]
+// '0x....'
+// { v: '0x01', r: '0x', s:'0x' }
+// SignatureData { _v: '0x01', _r: '0x', _s:'0x' }
+// [SignatureData { _v: '0x01', _r: '0x', _s:'0x' }]
 const isEmptySig = sig => {
-    if (!Array.isArray(sig)) sig = resolveSignature(sig)
+    let sigs = sig
 
-    function isEmpty(s) {
+    // Convert to array format
+    if (!_.isArray(sig)) sigs = resolveSignature(sigs)
+    // Format to two-dimentional array
+    if (_.isString(sigs[0])) sigs = [sigs]
+
+    for (let s of sigs) {
+        if (!_.isArray(s)) s = resolveSignature(s)
         if (s.length !== 3) throw new Error(`Invalid signatures length: ${s.length}`)
-
-        if (s[0] === '0x01' && s[1] === '0x' && s[2] === '0x') return true
-        return false
+        if (s[0] !== '0x01' || s[1] !== '0x' || s[2] !== '0x') return false
     }
 
-    if (_.isString(sig[0])) return isEmpty(sig)
-
-    if (sig.length !== 1) return false
-
-    return isEmptySig(sig[0])
+    return true
 }
 
 const hashMessage = data => {
