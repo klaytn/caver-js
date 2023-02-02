f1e0cea387d871dd58cedd401dc32a7f41f8c48d
Nohyun Nehemiah Kwak <nehemiah.kwak@krustuniverse.com>
2023-02-02 19:10:41 +0900

    Delete annotation

--- packages/caver-ipfs/src/index.js
+++ packages/caver-ipfs/src/index.js
@@ -57,18 +57,6 @@ class IPFS {
      * @param {object}  [options]   An object contains configuration variables.
      * @return {void}
      */
-    // async setIPFSNode(host, port, ssl, options = {}) {
-    //     const protocol = ssl ? 'https' : 'http'
-    //     // Use the dynamic import function to load ipfs at runtime
-    //     // refer to https://github.com/ipfs/js-ipfs/blob/master/docs/upgrading/v0.62-v0.63.md#esm
-    //     const { create } = await import('ipfs-http-client')
-    //     const createObject = { host, port, protocol }
-    //     if (options.headers) {
-    //         // Copy the headers object
-    //         createObject.headers = JSON.parse(JSON.stringify(options.headers))
-    //     }
-    //     this.ipfs = await create(createObject)
-    // }
     setIPFSNode(host, port, ssl, options = {}) {
         const protocol = ssl ? 'https' : 'http'
         const createObject = { host, port, protocol }
